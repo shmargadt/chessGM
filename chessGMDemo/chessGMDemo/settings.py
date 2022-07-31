efc6b4857f56d18cf13c88d352bd48c163292faf
Tomer Shmargad <tomer.shmargad@bluevine.com>
2022-07-31 16:43:03 +0300

    removing unused key

--- chessGMDemo/chessGMDemo/settings.py
+++ chessGMDemo/chessGMDemo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l8^b20s%pbtbs8gf5p+$ft3t2&#z+qi(h*xi+c+#!2(4o^zi&d'
+# SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
